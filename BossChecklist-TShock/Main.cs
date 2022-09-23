b37e64655ced960f7a17aefc7b684e7d1a6184c8
zqhong <zqh0513@gmail.com>
2022-09-23 21:14:55 +0800

    improve：完善 Boss 击杀输出格式；补充文档

--- BossChecklist-TShock/Main.cs
+++ BossChecklist-TShock/Main.cs
@@ -3,17 +3,25 @@ using System;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BossChecklist_TShock
 {
     [ApiVersion(2, 1)]
     public class BossChecklist_TShock : TerrariaPlugin
     {
-        public override string Author => "hdseventh";
+        public override string Author => "hdseventh and zqhong";
         public override string Description => "Boss Checklist for TShock";
         public override string Name => "Boss Checklist";
-        public override Version Version { get { return new Version(1, 0, 0, 0); } }
-        public BossChecklist_TShock(Main game) : base(game) { }
+
+        public override Version Version =>
+            new Version(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+        public BossChecklist_TShock(Main game) : base(game)
+        {
+        }
+
         public override void Initialize()
         {
             ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
@@ -35,42 +43,72 @@ namespace BossChecklist_TShock
         /// <param name="args"></param>
         public void bosscheck(CommandArgs args)
         {
-            var killed = "([c/FF0000:Killed])";
-            var alive = "([c/00FF00:Alive])";
-            StringBuilder sb = new StringBuilder();
+            var killedList = new List<string>();
+            var aliveList = new List<string>();
+
+            var bossMap = new Dictionary<string, bool>
+            {
+                { "克苏鲁之眼", NPC.downedBoss1 },
+                { "史莱姆王", NPC.downedSlimeKing },
+                { "骷髅王", NPC.downedBoss3 },
+                { "血肉墙", Main.hardMode },
+                { "史莱姆皇后", NPC.downedQueenSlime },
+                { "毁灭者", NPC.downedMechBoss1 },
+
+
+                { "双子魔眼", NPC.downedMechBoss2 },
+                { "机械骷髅王", NPC.downedMechBoss3 },
+                { "猪龙鱼公爵", NPC.downedFishron },
+                { "石巨人", NPC.downedGolemBoss },
+                { "世纪之花", NPC.downedPlantBoss },
+                { "光之女皇", NPC.downedEmpressOfLight },
+                { "拜月教邪教徒", NPC.downedAncientCultist },
+                { "月亮领主", NPC.downedMoonlord },
+                { WorldGen.crimson ? "克苏鲁之脑" : "世界吞噬怪", NPC.downedBoss2 }
+            };
+
+            foreach (var item in bossMap)
+            {
+                if (item.Value)
+                {
+                    killedList.Add(item.Key);
+                }
+                else
+                {
+                    aliveList.Add(item.Key);
+                }
+            }
+
+            var sb = new StringBuilder();
+
+
+            var killedStr = "无";
+            if (killedList.Count > 0)
+            {
+                killedStr = string.Join("，", killedList);
+            }
+
+            var aliveStr = "无";
+            if (aliveList.Count > 0)
+            {
+                aliveStr = string.Join("，", aliveList);
+            }
+
+            // const string killed = "([c/FF0000:Killed])";
+            // const string alive = "([c/00FF00:Alive])";
             
-            sb.Append("克苏鲁之眼 " + (NPC.downedBoss1 ? killed : alive));
-            sb.Append(", 史莱姆王 " + (NPC.downedSlimeKing ? killed : alive));
-            if (WorldGen.crimson)
-            {
-                sb.Append(", 克苏鲁之脑 " + (NPC.downedBoss2 ? killed : alive));
-            }
-            else
-            {
-                sb.Append(", 世界吞噬怪 " + (NPC.downedBoss2 ? killed : alive));
-            }
-            sb.Append(", 骷髅王 " + (NPC.downedBoss3 ? killed : alive));
-            sb.Append(", 血肉墙 " + (Main.hardMode ? killed : alive));
-            sb.Append(", 史莱姆皇后 " + (NPC.downedQueenSlime ? killed : alive));
-            sb.Append(", 毁灭者 " + (NPC.downedMechBoss1 ? killed : alive));
-            sb.Append(", 双子魔眼 " + (NPC.downedMechBoss2 ? killed : alive));
-            sb.Append(", 机械骷髅王 " + (NPC.downedMechBoss3 ? killed : alive));
-            sb.Append(", 猪龙鱼公爵 " + (NPC.downedFishron ? killed : alive));
-            sb.Append(", 石巨人 " + (NPC.downedGolemBoss ? killed : alive));
-            sb.Append(", 世纪之花 " + (NPC.downedPlantBoss ? killed : alive));
-            sb.Append(", 光之女皇 " + (NPC.downedEmpressOfLight ? killed : alive));
-            sb.Append(", 拜月教邪教徒 " + (NPC.downedAncientCultist ? killed : alive));
-            sb.Append(", 月亮领主 " + (NPC.downedMoonlord ? killed : alive));
-            args.Player.SendInfoMessage(sb.ToString());
+            args.Player.SendInfoMessage($"[c/FF0000:已打败]：{killedStr}");
+            args.Player.SendInfoMessage($"[c/00FF00:未击杀]：{aliveStr}");
         }
 
-        protected override void Dispose(bool Disposing)
+        protected override void Dispose(bool disposing)
         {
-            if (Disposing)
+            if (disposing)
             {
                 ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
             }
-            base.Dispose(Disposing);
+
+            base.Dispose(disposing);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 # BossChecklist-TShock
+
 Boss Checklist for TShock.
 
+![preview_20220923210731.jpg](https://s2.loli.net/2022/09/23/54icFLCPAwoN37M.jpg)
+
 ## Permission
+
 - `bcheck.use` Permission to use the /bosses command.
 
 ## Command
+
 - `/bosses` `Permission Required : bcheck.use`  Main command.
+
+## 插件安装
+
+将 `BossChecklist-TShock.dll` 放置在 TShock 的插件目录（比如 `~/terraria/ServerPlugins`）下，重启即可。
+
+## 开发
+
+将 TShock 服务端相关的文件放置到 `packages` 目录下。需要的文件如下：
+
+* OTAPI.dll
+* TerrariaServer.exe
+* TShockAPI.dll
+
+我的开发环境：
+
+```text
+Terraria Server v1.4.3.6
+TerrariaAPI Version: 2.1.0.0 (Protocol v1.4.3.6 (248), OTAPI 1.4.3.6)
+```
